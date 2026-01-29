1d9b61b7baa69f2bcd162673b0620b9d66dd1cdd
Richemond Jagoum <jagoum24@gmail.com>
2026-01-29 16:15:23 +0100

    fix: fixed spelling issue

--- lgtm-stack/terraform/variables.tf
+++ lgtm-stack/terraform/variables.tf
@@ -3,7 +3,7 @@ variable "cloud_provider" {
   type        = string
   default     = "gke"
   validation {
-    condition     = contains(["gke", "eks", "generic"], var.c loud_provider)
+    condition     = contains(["gke", "eks", "generic"], var.cloud_provider)
     error_message = "The cloud provider must be one of the following: gke, eks, or generic."
   }
 }
